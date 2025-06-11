86160ac1e6f0382345dcc8886d80993003027e4b
lenovo <ssopheakline@gmail.com>
2025-06-11 10:40:56 +0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const container = document.querySelector(".container")
+const coffees = [
+  { name: "Perspiciatis", img: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=400&q=80" },
+  { name: "Voluptatem", img: "https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=400&q=80" },
+  { name: "Explicabo", img: "https://images.unsplash.com/photo-1511920170033-f8396924c348?auto=format&fit=crop&w=400&q=80" },
+  { name: "Rchitecto", img: "https://images.unsplash.com/photo-1519125323398-675f0ddb6308?auto=format&fit=crop&w=400&q=80" },
+  { name: "Beatae", img: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=400&q=80" },
+  { name: "Vitae", img: "https://images.unsplash.com/photo-1465101178521-c1a9136a3b99?auto=format&fit=crop&w=400&q=80" },
+  { name: "Inventore", img: "https://images.unsplash.com/photo-1517685352821-92cf88aee5a5?auto=format&fit=crop&w=400&q=80" },
+  { name: "Veritatis", img: "https://images.unsplash.com/photo-1468071174046-657d9d351a40?auto=format&fit=crop&w=400&q=80" },
+  { name: "Accusantium", img: "https://images.unsplash.com/photo-1504639725590-34d0984388bd?auto=format&fit=crop&w=400&q=80" },
+]
+
+const showCoffees = () => {
+    let output = ""
+    coffees.forEach(
+      ({ name, img }) =>
+        (output += `
+                <div class="card">
+                  <img class="card--img" src="${img}" alt="${name} coffee" />
+                  <h1 class="card--title">${name}</h1>
+                  <a class="card--link" href="#">Taste</a>
+                </div>
+                `)
+    )
+    container.innerHTML = output
+  }
+  
+  document.addEventListener("DOMContentLoaded", showCoffees)
+  
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }

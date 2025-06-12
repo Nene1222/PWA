96fa82e8d119eadffdee33000b59e3c59e2736bd
Sambo Sopheakline <ssopheakline@gmail.com>
2025-06-12 14:15:03 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const coffees = [
   { name: "Explicabo", img: "https://images.unsplash.com/photo-1511920170033-f8396924c348?auto=format&fit=crop&w=400&q=80" },
   { name: "Rchitecto", img: "https://images.unsplash.com/photo-1519125323398-675f0ddb6308?auto=format&fit=crop&w=400&q=80" },
   { name: "Beatae", img: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=400&q=80" },
-  { name: "Vitae", img: "https://images.unsplash.com/photo-1465101178521-c1a9136a3b99?auto=format&fit=crop&w=400&q=80" },
+  { name: "Vitae", img: "https://images.unsplash.com/photo-1519125323398-675f0ddb6308?auto=format&fit=crop&w=400&q=80" },
   { name: "Inventore", img: "https://images.unsplash.com/photo-1517685352821-92cf88aee5a5?auto=format&fit=crop&w=400&q=80" },
   { name: "Veritatis", img: "https://images.unsplash.com/photo-1468071174046-657d9d351a40?auto=format&fit=crop&w=400&q=80" },
   { name: "Accusantium", img: "https://images.unsplash.com/photo-1504639725590-34d0984388bd?auto=format&fit=crop&w=400&q=80" },
